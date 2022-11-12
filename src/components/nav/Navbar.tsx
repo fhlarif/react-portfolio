418a4ea49d44a626835ed600d04d8eb8d70c44ef
fathularif <fathulcode@gmail.com>
2022-11-12 14:38:16 +0800

    update Navbar

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,13 +1,25 @@
-import Resume from "../contents/Resume";
+import { ReactNode } from "react";
 
-const Navbar = () => {
+type Props = {
+  children?: ReactNode;
+};
+
+const Li = ({ children }: Props) => {
   return (
-    <nav className="mt-4 p-4 bg-gray-700 flex h-14 justify-between w-full ">
-      <span>Profile1</span>
-      <span>Profile2</span>
-      <span>Profile3</span>
-    </nav>
+    <li className="hover:underline hover:decoration-amber-500 underline-offset-8 hover:text-amber-500 ">{children}</li>
   );
+};
+
+const Ul: JSX.Element = (
+  <ul className="flex md:justify-between w-full md:flex-row items-center flex-col justify-center">
+    <Li>Home</Li>
+    <Li>Resume</Li>
+    <Li>Resume</Li>
+  </ul>
+);
+
+const Navbar = (): JSX.Element => {
+  return <nav className="mt-4 p-4 bg-gray-700  flex h-14 items-center w-full ">{Ul}</nav>;
 };
 
 export default Navbar;

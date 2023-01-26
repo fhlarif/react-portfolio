1f230dfcaae288635d801cfc955c65b39e8b10fc
fathularif <fathulcode@gmail.com>
2023-01-26 22:51:21 +0800

    remove some comment

--- src/components/contents/Portfolio.tsx
+++ src/components/contents/Portfolio.tsx
@@ -98,11 +98,6 @@ const Portfolio = () => {
               <a
                 href="#"
                 className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                {/* <img
-                  className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                  src={"/assets/black_bk6.jpg"}
-                  alt=""
-                /> */}
                 <div className="flex flex-col justify-between p-4 leading-normal">
                   <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-amber-500">
                     {p.title}
